f8e45a74aae4d940e7c7378fdae65739c251e20e
Lucas Reis <lucasreis_cod@hotmail.com>
2022-05-29 19:51:42 -0300

    Criado a barra de naveggação da pagina home.php e feita etapas sde segurança para ser acessada apenas a pagina home quand oestiver logado

--- home.php
+++ home.php
@@ -1,14 +1,58 @@
+<?php
+require 'conn.php';
+
+if(isset($_SESSION['idUser']) && !empty($_SESSION['idUser'])):
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <title>Meu Sistema</title>
 </head>
 <body>
 
-    <h1> Seja Bem Vindo ao Sistema</h1>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LOJA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cadastro
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Cliente</a></li>
+            <li><a class="dropdown-item" href="#">Produtos</a></li>
+          </ul>
+        </li>
+      </ul>
+        <div class="form-inline my-2 my-lg-0 btn" >
+            <a href="logout.php">SAIR</a>
+        </div>
+    </div>
+  </div>
+</nav>
     
+    <!-- JavaScript Bundle with Popper -->  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<?php
+else: header ("Location:login.html"); endif; ?>
