2b22e4734e79a53b51988ebc1a4750688198e65d
Lucas Reis <lucasreis_cod@hotmail.com>
2022-05-30 08:29:07 -0300

    Criada a mensagem de Boas vindas

--- home.php
+++ home.php
@@ -20,40 +20,46 @@ if(isset($_SESSION['idUser']) && !empty($_SESSION['idUser'])):
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">LOJA</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Cadastro
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Cliente</a></li>
-            <li><a class="dropdown-item" href="#">Produtos</a></li>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">LOJA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cadastro
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Cliente</a></li>
+                <li><a class="dropdown-item" href="#">Produtos</a></li>
+              </ul>
+            </li>
           </ul>
-        </li>
-      </ul>
-        <div class="form-inline" > <!--my-lg-0 -->
-            <label class="p-2"><?php echo $nomeUser ?></label>
-            <a href="logout.php" class="btn mr-2">SAIR</a>
+            <div class="form-inline" > <!--my-lg-0 -->
+                <label class="p-2"><?php echo $nomeUser ?></label>
+                <a href="logout.php" class="btn mr-2">SAIR</a>
+            </div>
         </div>
-    </div>
-  </div>
-</nav>
-    
+      </div>
+    </nav>
+
+    <main>
+      <div class="container-fluid">
+        <h3 class="mt-3">Olá <?php echo $nomeUser ?>, Seja Bem Vindo </h3>
+      </div>
+    </main>  
+
     <!-- JavaScript Bundle with Popper -->  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
-
+  
 <?php
 else: header ("Location:login.html"); endif; ?>
